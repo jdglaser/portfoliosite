2a3cfde695bf29c1e09da66d820fb7b813a7154c
jdglaser <jarred.glaser@gmail.com>
2018-11-22 09:44:40 -0600

    fixed smooth scroll for unsuported browsers

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Raise items
 $('.raise-item').hover(
     function(){ $(this).addClass('shadow-lg p-3 mb-5 bg-white rounded') },
     function(){ $(this).removeClass('shadow-lg p-3 mb-5 bg-white rounded').addClass('shadow-sm p-3 mb-5 bg-white rounded') }
 )
 
+// Panel collapsing
 $(".panel-heading").hover(
     function() {
       $(this).children('.panel').children('.collapse').collapse('show');
@@ -11,13 +13,39 @@ $(".panel-heading").hover(
     }
   );
 
-// When the user scrolls down 20px from the top of the document, show the button
+
+// Scroll back to top btn
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
         document.getElementById("topbtn").style.display = "block";
     } else {
         document.getElementById("topbtn").style.display = "none";
     }
 }
+
+// Smooth Scroll for browsers that don't support scroll-behavior css
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
