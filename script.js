053d1b7a4542d9b148152526eaa3bb923d170b7a
jdglaser <jarred.glaser@gmail.com>
2018-11-22 08:50:39 -0600

    added experience

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ $('.raise-item').hover(
     function(){ $(this).addClass('shadow-lg p-3 mb-5 bg-white rounded') },
     function(){ $(this).removeClass('shadow-lg p-3 mb-5 bg-white rounded').addClass('shadow-sm p-3 mb-5 bg-white rounded') }
 )
+
+$(".panel-heading").hover(
+    function() {
+      $(this).children('.panel').children('.collapse').collapse('show');
+    }, function() {
+      $(this).children('.panel').children('.collapse').collapse('hide');
+    }
+  );
