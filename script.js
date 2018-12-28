677233f8ff45aae37adb0ccfb9bc2160bde6736e
jdglaser <jarred.glaser@gmail.com>
2018-12-28 16:00:04 -0600

    adding sills section

--- script.js
+++ script.js
@@ -83,3 +83,7 @@ $('.raise-item').hover(
   function(){ $(this).addClass('shadow-lg p-3 mb-5 bg-white rounded') },
   function(){ $(this).removeClass('shadow-lg p-3 mb-5 bg-white rounded').addClass('shadow-sm p-3 mb-5 bg-white rounded') 
 })
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
